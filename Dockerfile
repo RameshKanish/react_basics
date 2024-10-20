2ea4b2e1dc7a3889304785be50bd4ed47d65a41b
Ramesh Rs <rameshrs@centizen.com>
2024-10-20 18:30:48 +0530

    Remove docker file

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM maven:3.8.5-openjdk-17 AS build
-COPY . .
-RUN mvn clean package -DskipTests
-
-
-FROM openjdk:17.0.1-jdk-slim
-EXPOSE 8080
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0', // Bind to 0.0.0.0 to allow external access
+    // eslint-disable-next-line no-undef
     port: process.env.PORT || 5173, // Use environment PORT or default to 5173
   },
 })
