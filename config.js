93cb0f376e70bae3c1a37f1fc25a237d38a11d3b
Ramesh Rs <rameshrs@centizen.com>
2025-01-08 15:09:37 +0530

    Changes

--- config.js
+++ config.js
@@ -1,2 +1,4 @@
 // config.js
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:9000';
+export default {
+  VITE_API_BASE_URL: 'https://bookmyshow-backend-latest.onrender.com'
+};
